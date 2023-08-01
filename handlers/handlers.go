89848a88da1ed96865e6775adbc1908b273d3ae2
garima-saxena-0702 <garimasaxena0702@gmail.com>
2023-08-01 16:43:54 +0530

    initial commit

---
+++ handlers/handlers.go
@@ -0,0 +1,134 @@
+package handlers
+
+import (
+    "fmt"
+	"strconv"
+	"encoding/json"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"splitwise/splitwisedb"
+	"splitwise/schemas"
+)
+
+var dbConnection *splitwisedb.SplitwiseDB
+
+func init() {
+	dbConnection = splitwisedb.CreateDB()
+}
+
+func GetTransactions (c *gin.Context){
+	var gid, uid int
+	sgid, gok := c.GetQuery("groupid")
+	//fromDate, fok := c.GetQuery("fromDate")
+	//toDate, tok := c.GetQuery("toDate")
+	if !gok {
+		gid = -1
+	} else {
+		gid, _ = strconv.Atoi(sgid);
+	}
+	suid, uok := c.GetQuery("userid")
+	if !uok {
+		panic("Need user Id")
+	} else {
+		uid, _ = strconv.Atoi(suid); 
+	}
+
+	var txns = dbConnection.SelectTransctions(gid)
+	result := make(map[int]schemas.Result)
+	for _, txn := range *txns {
+		println(txn.Tid, txn.Groupid, txn.Desc, txn.Creator, txn.Totalamount, txn.Owee, txn.Pendingamount)
+		groupname := dbConnection.SelectGroups(txn.Groupid)
+		var res schemas.Result;
+		if txn.Creator == uid {
+			t, ok := result[txn.Tid]
+			if ok {
+				println("at uid old")
+				res = schemas.Result{
+					Date: calculateTime(txn.Date),
+					Group: groupname,
+					Desc: txn.Desc,
+					Totalamount: txn.Totalamount,
+					Pendingamount: txn.Pendingamount + t.Pendingamount,
+				}
+				result[txn.Tid] = res
+			} else {
+				println("At uid new")
+				res = schemas.Result{
+					Date: calculateTime(txn.Date),
+					Group: groupname,
+					Desc: txn.Desc,
+					Totalamount: txn.Totalamount,
+					Pendingamount: txn.Pendingamount,
+				}
+				result[txn.Tid] = res;
+			}
+		} else if txn.Owee == uid {
+			println("at oweee")
+			res = schemas.Result{
+				Date: calculateTime(txn.Date),
+				Group: groupname,
+				Desc: txn.Desc,
+				Totalamount: txn.Totalamount,
+				Pendingamount: -txn.Pendingamount,
+			}
+			result[txn.Tid] = res;
+		}
+	} 
+	a, _ := json.Marshal(result)
+	fmt.Println(string(a))
+
+	c.JSON(200, gin.H{
+		"message": string(a),
+	})
+}
+
+func GetUsers (c *gin.Context){
+	users := dbConnection.SelectUsers()
+	a, _ := json.Marshal(users)
+
+	c.JSON(200, gin.H{
+		"message": string(a),
+	})
+}
+
+func CreateUser (c *gin.Context){
+	var user schemas.User
+	if err := c.BindJSON(&user); err != nil {
+        panic(err)
+    }
+
+	fmt.Printf("Got user - %d %s\n", user.Id, user.Name)
+	dbConnection.InsertUser(user);
+	c.JSON(200, gin.H{
+		"message": "Row Inserted",
+	})
+}
+
+func CreateGroup (c *gin.Context){
+	var groups []schemas.Group
+	if err := c.BindJSON(&groups); err != nil {
+        panic(err)
+    }
+	dbConnection.InsertGroup(groups);
+	c.JSON(200, gin.H{
+		"message": "Row Inserted",
+	})
+}
+
+func CreateTransaction (c *gin.Context){
+	var txns []schemas.Transactions
+	if err := c.BindJSON(&txns); err != nil {
+        panic(err)
+    }
+
+    dbConnection.InsertTransactions(txns);
+	c.JSON(200, gin.H{
+		"message": "Row Inserted",
+	})
+}
+
+func calculateTime (date time.Time) string {
+	y, m, d := date.Date()
+	return strconv.Itoa(d) + "-" + m.String() + "-" + strconv.Itoa(y)   
+}
---
+++ splitwisedb/db.go
@@ -0,0 +1,152 @@
+package splitwisedb
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+	
+	"splitwise/schemas"
+)
+
+type SplitwiseDB struct {
+	connection *sql.DB
+}
+
+func CreateDB() *SplitwiseDB {
+	connStr := "user=setu dbname=splitwisedb password=password host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	//db, err := sql.Open("postgres", "dbname=splitwisedb user=setu password=password")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &SplitwiseDB{connection: db}
+}
+
+func (db SplitwiseDB) Ping() {
+	err := db.connection.Ping()
+	if err == nil {
+		println("pinged!")
+	} else {
+		log.Fatal(err)
+	}
+}
+
+func (db SplitwiseDB) SelectUsers() *[]schemas.User {
+	rows, err := db.connection.Query("SELECT * FROM users")
+    if err != nil {
+		println("Error!")
+        log.Fatal(err)
+    }
+    defer rows.Close()
+    
+	var users []schemas.User 
+	for rows.Next() {
+		var user schemas.User
+        err := rows.Scan(&user.Id, &user.Name)
+        if err != nil {
+            panic(err)
+        }
+		users = append(users, user)
+	}
+
+	return &users
+}
+
+func (db SplitwiseDB) InsertUser(user schemas.User) {
+	sqlStr := fmt.Sprintf("INSERT INTO USERS VALUES (%d, '%s')", user.Id, user.Name)
+	_, err := db.connection.Query(sqlStr)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (db SplitwiseDB) InsertGroup(groups []schemas.Group) {
+	sqlStr := "INSERT INTO GROUPS VALUES "
+	for i, group := range groups {
+		sqlStr += fmt.Sprintf("(%d, '%s', %d)",group.Groupid, group.Name, group.Member)
+		if i < (len(groups) - 1) {
+			sqlStr += ","
+		}
+	}
+	println(sqlStr)
+	_, err := db.connection.Query(sqlStr)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (db SplitwiseDB) InsertTransactions(txns []schemas.Transactions) {
+	sqlStr := "INSERT INTO TRANSACTIONS VALUES "
+	for i, txn := range txns {
+		sqlStr += fmt.Sprintf("(%d, %d, '%s', '%d%02d%02d %02d:%02d:%02d', %d, %d, %d, %d)", 
+							txn.Tid, txn.Groupid, txn.Desc, 
+							txn.Date.Year(), txn.Date.Month(), txn.Date.Day(), txn.Date.Hour(), 
+						    txn.Date.Minute(), txn.Date.Second(), txn.Creator,
+						    txn.Totalamount, txn.Owee, txn.Pendingamount)
+		if i < (len(txns) - 1) {
+			sqlStr += ","
+		}
+	}
+
+	println(sqlStr)
+	_, err := db.connection.Query(sqlStr)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (db SplitwiseDB) SelectGroups(groupid int) string {
+	sqlStr := fmt.Sprintf("SELECT * FROM Groups where groupid=%d", groupid)
+	rows, err := db.connection.Query(sqlStr)
+		if err != nil {
+			println("Error!")
+			log.Fatal(err)
+		}
+		defer rows.Close()
+		
+		for rows.Next() {
+			var group schemas.Group
+			err := rows.Scan(&group.Groupid, &group.Name, &group.Member)
+			if err != nil {
+				panic(err)
+			}
+			return group.Name
+		}
+		return ""
+}
+
+
+func (db SplitwiseDB) SelectTransctions(gid int) *[]schemas.Transactions {
+	var sqlStr string
+	if gid != -1 {
+		sqlStr = fmt.Sprintf("SELECT * FROM TRANSACTIONS WHERE groupid=%d", gid)
+	} else {
+		sqlStr = "SELECT * FROM TRANSACTIONS"
+	}
+
+	rows, err := db.connection.Query(sqlStr)
+    if err != nil {
+		println("Error!")
+        log.Fatal(err)
+    }
+    defer rows.Close()
+
+	var txns []schemas.Transactions
+	for rows.Next() {
+		var txn schemas.Transactions
+        err := rows.Scan(&txn.Tid, &txn.Groupid, &txn.Desc, &txn.Date, &txn.Creator, &txn.Totalamount, &txn.Owee, &txn.Pendingamount)
+        if err != nil {
+            panic(err)
+        }
+		txns = append(txns, txn)
+	}
+
+	return &txns
+
+}
